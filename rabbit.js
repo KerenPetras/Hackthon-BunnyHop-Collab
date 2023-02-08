e20824c36e6a4dd0f4a43468e745d3fd1088ccf0
Keren Petras <112869651+KerenPetras@users.noreply.github.com>
2023-02-08 18:20:15 +0200

    Update rabbit.js

--- rabbit.js
+++ rabbit.js
@@ -67,7 +67,7 @@ function onHop(evt) {
     if (evt.code !== 'Space' || isHopping) return
     yVelocity = HOP_SPEED
     isHopping = true
-    hopSound.volume = 0.5;
+    hopSound.volume = 0.2;
     hopSound.currentTime = 0;
     hopSound.play();
 }
